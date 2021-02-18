976b02ac5d6dc73f0f030c2c72b90b5214ffb99b
Cameron A Zimmerman <cameronazimmerman@gmail.com>
2021-02-18 14:17:20 -0800

    create bros and bros/:name endpoint tests

--- __tests__/get.tests.js
+++ __tests__/get.tests.js
@@ -3,10 +3,39 @@ const supertest = require('supertest');
 const request = supertest(app);
 const { mario_bros_data } = require('../data.js');
 
-it('gets the test endpoint', async done => {
+it('gets the hom endpoint and checks response', async done => {
 
     const expectedData = { message: 'Welcome to my mario bros api! Enjoy'}
     const response = await request.get('/');
+
+    expected(response.status).toBe(200);
+    expected(response.body).toEqual(expectedData);
+    done();
+});
+
+it('gets the bros endpoint and tests response', async done => {
+
+    const expectedData = { response: mario_bros_data}
+    const response = await request.get('/bros');
+
+    expected(response.status).toBe(200);
+    expected(response.body).toEqual(expectedData);
+    done();
+});
+
+it('gets the bros/:name endpoint and tests response', async done => {
+
+    const luigi = {
+        name: 'luigi',
+        good_guy: true,
+        image_url: 'https://www.mariowiki.com/images/b/b7/SMB_Super_Luigi_Sprite.png',
+        age: 43,
+        color: 'green',
+        quote: 'Luigi time!'
+    };
+
+    const expectedData = {response: luigi};
+    const response = await request.get('/bros/luigi');
 
     expected(response.status).toBe(200);
     expected(response.body).toEqual(expectedData);
