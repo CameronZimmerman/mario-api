3f54d4db5d1ac9ff8c2c08a4c58a2fc80b8fcf94
Cameron A Zimmerman <cameronazimmerman@gmail.com>
2021-02-18 14:01:25 -0800

    create mario_bros data

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # mario-api
+
+- 1) create mario bros data(string, num, bool, dropdown(type attribute etc)):
+    - name: 'mario',
+    - good_guy: true,
+    - image_url: 'https://somemarioimage.jpg',
+    - age: n,
+    - color: red,
+    - quote: 'it's a me, mario'
+
+- 2) write tests for endpoints based off of data
+- 3) create 3 endpoints
+    - home or "/" (simple greeting or welcome)
+    - "/bros" lists all mario brothers/characters
+    - "/bros/:name"
+4) publish to heroku??
---
+++ data.js
@@ -0,0 +1,56 @@
+const mario_bros_data = [
+    {
+        name: 'mario',
+        good_guy: true,
+        image_url: 'https://www.mariowiki.com/images/c/cf/SMB_Super_Mario_Sprite.png',
+        age: 45,
+        color: 'red',
+        quote: 'It\'s a-me, Mario!'
+    },
+    {
+        name: 'luigi',
+        good_guy: true,
+        image_url: 'https://www.mariowiki.com/images/b/b7/SMB_Super_Luigi_Sprite.png',
+        age: 43,
+        color: 'green',
+        quote: 'Luigi time!'
+    },
+    {
+        name: 'princess toadstool',
+        good_guy: true,
+        image_url: 'https://www.mariowiki.com/images/c/c0/SMB_Princess_Toadstool_Sprite.png',
+        age: 40,
+        color: 'pink',
+        quote: 'Oh, no... oh.'
+    },
+    {
+        name: 'goomba',
+        good_guy: false,
+        image_url: 'https://www.mariowiki.com/images/4/4e/SMB_Goomba_Sprite.gif',
+        age: 'N/A',
+        color: 'brown',
+        quote: 'N/A'
+    },
+    {
+        name: 'bullet bill',
+        good_guy: false,
+        image_url: 'https://www.mariowiki.com/images/e/ec/Bullet_Bill_Super_Mario_Bros.png',
+        age: 'N/A',
+        color: 'black',
+        quote: 'N/A'
+    },
+    {
+        name: 'bowser',
+        good_guy: false,
+        image_url: 'https://www.mariowiki.com/images/3/31/SMB_Bowser_Walking_Sprite.gif',
+        age: 50,
+        color: 'green',
+        quote: 'Mario! Prepare yourself for the great beyond!'
+    },
+
+
+]
+
+module.exports = {
+
+}
