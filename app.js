1f5a7df12503774de7a7889f791797e4c904ab57
Cameron A Zimmerman <cameronazimmerman@gmail.com>
2021-02-18 14:24:52 -0800

    finish bros endpoint

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
+const { mario_bros_data } = require('./data.js');
 
 app.get('/', (req, res) => {
     res.json({ message: 'Welcome to my mario bros api! Enjoy.'} );
+})
+
+app.get('/bros', (req, res) => {
+    res.json({ response: mario_bros_data} );
 })
 
 module.exports = {
