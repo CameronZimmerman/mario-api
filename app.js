b2d0ed9d8dc2c7529e3482829400818a26d9fbeb
Cameron A Zimmerman <cameronazimmerman@gmail.com>
2021-02-18 14:30:10 -0800

    finish specific mario bro route

--- app.js
+++ app.js
@@ -4,11 +4,19 @@ const { mario_bros_data } = require('./data.js');
 
 app.get('/', (req, res) => {
     res.json({ message: 'Welcome to my mario bros api! Enjoy.'} );
-})
+});
 
 app.get('/bros', (req, res) => {
     res.json({ response: mario_bros_data} );
-})
+});
+
+app.get('/bros/:name', (req, res) => {
+    let name = req.params.name;
+
+    let mario_bros_obj = mario_bros_data.find(data_obj => data_obj.name === name);
+
+    res.json({response: mario_bros_obj});
+});
 
 module.exports = {
     app
